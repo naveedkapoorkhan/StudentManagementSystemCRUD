97ccb8e4b6f5e879b453d12680b23d9ae7b427cc
naveedkapoorkhan <naveedkapoorkhan@gmail.com>
2025-09-01 19:16:00 +0500

    add student page in layout.cshtml link connect db create controllers

--- StudentManagementSystemCRUD/Data/AppDbContext.cs
+++ StudentManagementSystemCRUD/Data/AppDbContext.cs
@@ -1,16 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using StudentManagementSystemCRUD.Models;
 
-namespace StudentManagement.Data
+namespace StudentManagementSystemCRUD.Data
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
-        public DbSet<Student> Students { get; set; }  // Table Students
+        //adding student table class AppDbContext
+        public DbSet<Student> Students { get; set; }  // Table Students it defines a table in EF. EF will handle table creation when you apply migrations.
     }
 }
-
+//DbContext is the bridge between your C# classes and the database:
 //DbContext = bridge between C# and database.
 
 //DbSet < Student > = represents the Students table.
